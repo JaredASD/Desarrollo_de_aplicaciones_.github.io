b693edd22a4843f1f31a8dc31e356ea28325759f
JaredASD <t01298h@ms.upla.edu.pe>
2025-09-12 19:09:27 -0500

    Add files via upload

---
+++ Semana01/Semana01_tarea_1/src/main/java/com/mycompany/tarea_aplicacion/Tarea_aplicacion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_aplicacion;
+import Vista.tarea;
+/**
+ *
+ * @author User
+ */
+public class Tarea_aplicacion {
+
+    public static void main(String[] args) {
+       tarea tar = new tarea();
+        tar.show();
+    }
+}
