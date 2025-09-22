88841e6149722b0216aa7e67f5d1a9e8baac2f7a
JaredASD <t01298h@ms.upla.edu.pe>
2025-09-21 22:14:30 -0500

    Add files via upload

---
+++ Semana03/Semana03_tarea_2/src/main/java/com/mycompany/semana2_tarea_2/Semana2_tarea_2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana2_tarea_2;
+
+import Vista.Semana2_tarea_2_vista;
+/**
+ *
+ * @author User
+ */
+public class Semana2_tarea_2 {
+
+    public static void main(String[] args) {
+        Semana2_tarea_2_vista ejem = new Semana2_tarea_2_vista();
+        ejem.show();
+    }
+}
